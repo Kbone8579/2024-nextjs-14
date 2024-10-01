b53f2b6ff1c24164c12e8d8e528edf86e7818d13
Kbone8579 <a01056718579@gmail.com>
2024-10-02 00:26:08 +0900

    work

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
+import { faArrowUpWideShort } from "@fortawesome/free-solid-svg-icons/faArrowUpWideShort";
 
 export default function Home() {
   return (
     <div className="text-blue-500">
       <h1>홈 페이지!!!???asdsadsadsad</h1>
-      <FontAwesomeIcon icon={faArrowRight}/>
+      <FontAwesomeIcon icon={faArrowUpWideShort} />
     </div>
   );
 }
